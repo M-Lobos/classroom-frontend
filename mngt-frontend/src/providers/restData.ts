264747b9caea000af8892565dd00f0cd74f5ef62
M-Lobos <manuel.lobos.88@gmail.com>
2026-06-11 19:40:44 -0400

    ft: Rate limit toast implemented through Arcjet

--- mngt-frontend/src/providers/restData.ts
+++ mngt-frontend/src/providers/restData.ts
@@ -1,10 +1,30 @@
 import { BACKEND_BASE_URL } from "@/constants"
 import { ListResponse } from "@/types/types";
+import { HttpError } from "@refinedev/core";
 import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest"
 
 //.env backend url validation
 if (!BACKEND_BASE_URL) {
     throw new Error('BACKEND_BASE_URL is not properly configured.Plase set BACK_BASE_URL in your .env file.')
+}
+
+//this will be used when mapping the responses; mapResponse() function
+const buildHttpError = async (response: Response): Promise<HttpError> => {
+    let message = 'Request failed';
+
+    try {
+
+        const payload = (await response.json()) as { message?: string }
+
+        if (payload.message) message = payload.message;
+    } catch {
+        //ignore errors
+    }
+
+    return {
+        message,
+        statusCode: response.status,
+    }
 }
 
 const options: CreateDataProviderOptions = {
@@ -32,12 +52,17 @@ const options: CreateDataProviderOptions = {
         },
 
         mapResponse: async (response) => {
+
+            if (!response.ok) throw await buildHttpError(response);
+
             const payload: ListResponse = await response.clone().json();
 
             return payload.data ?? [];
         },
 
         getTotalCount: async (response) => {
+            if (!response.ok) throw await buildHttpError(response);
+
             const payload: ListResponse = await response.clone().json();
 
             return payload.pagination?.total ?? payload.data?.length ?? 0;
