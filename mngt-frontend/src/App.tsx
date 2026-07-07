fd5da1c7db413550d3e2f96481a100d504447819
M-Lobos <manuel.lobos.88@gmail.com>
2026-07-07 14:29:29 -0400

    ft: correction on resources path ont Regine component

--- mngt-frontend/src/App.tsx
+++ mngt-frontend/src/App.tsx
@@ -69,6 +69,7 @@ function App() {
                   name: 'departments',
                   list: '/departments',
                   create: '/departments/create',
+                  show: '/classes/show/:id',
                   meta: {
                     label: 'Departments',
                     icon: <Building2 />
